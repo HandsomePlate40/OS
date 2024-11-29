1efdb492aa89862c6b354c30811fa49fe002b55d
Fady Osama <fady4321@outlook.com>
2024-11-30 00:35:33 +0200

    ..

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,5 @@
 import Cores.*;
-import Memory.Memory;
-import Parse.*;
 import Parse.ProgramParser;
-import Process_Related.Process;
-import Process_Related.ProcessControlBlock;
-import Queue.ReadyQueue;
 
 public class Main {
     public static void main(String[] args) {
